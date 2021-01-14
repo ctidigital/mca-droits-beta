b152566373f34400b0bbf46eaad9980f234dd597
ste-holmes <s.holmes@ctidigital.com>
2021-01-14 23:17:08 +0000

    Create pages for 2 possible answers to 'removed property check' page. Included necessary utility functions in server script to store and post checkbox value - ticket-ref: MCA-31

--- routes/removed-property-check.js
+++ routes/removed-property-check.js
@@ -7,4 +7,30 @@ router.get('/', function(req, res, next) {
   res.render('pages/removed-property-check.html')
 });
 
+router.post('/report/removed-property-check-answer', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const value = req.session.data['removed-property'];
+
+  // Throw the current date in here as it's a good a place as any! 
+  var date = new Date();
+  var day = date.getDate(),
+    month = date.getMonth() + 1,
+    year = date.getFullYear();
+
+  // We add 0 to the start of the date and slice the last two digits off to ensure leading zeros.
+  req.session.data['report-date'] = {};
+  req.session.data['report-date']['day'] = ("0" + day).slice(-2);
+  req.session.data['report-date']['month'] = ("0" + month).slice(-2);
+  req.session.data['report-date']['year'] = year;
+
+  if (value === 'yes') {
+    res.redirect('/pages/find-date')
+  } else {
+    res.redirect('/pages/not-removed-property-content')
+  }
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -8,9 +8,18 @@ const startRoute = require('./routes/start')
 const removedPropertyCheckRoute = require('./routes/removed-property-check')
 const path = require('path')
 const nunjucks = require('nunjucks')
+const sessionInMemory = require('express-session')
 
 // Local dependencies
-const config = require('./app/scripts/config.js')
+const config = require('./app/src/config.js')
+const utils = require('./lib/utilities/utils.js')
+
+// Configuration variables
+var useAutoStoreData = process.env.USE_AUTO_STORE_DATA || config.useAutoStoreData
+
+//var isSecure = (env === 'production' && useHttps === 'true')
+// PLACEHOLDER SET TO TRUE - REVIEW CODE IN PROTOTYPE
+var isSecure = true
 
 const app = express()
 
@@ -23,7 +32,7 @@ app.set('views','./app/views');
 // Nunjucks config
 app.set("view engine", "html")
 
-nunjucks.configure([
+var nunjucksAppEnv = nunjucks.configure([
   "node_modules/govuk-frontend/",
   'app/views/'
 ],
@@ -34,6 +43,28 @@ nunjucks.configure([
 
 // Add variables that are available in all views
 app.locals.serviceName = config.serviceName
+
+// Session uses service name to avoid clashes with other prototypes
+const sessionName = 'row-' + (Buffer.from(config.serviceName, 'utf8')).toString('hex')
+const sessionOptions = {
+  secret: sessionName,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 4, // 4 hours
+    secure: isSecure
+  }
+}
+
+// Support session data in memory
+app.use(sessionInMemory(Object.assign(sessionOptions, {
+  name: sessionName,
+  resave: false,
+  saveUninitialized: false
+})))
+
+if (useAutoStoreData === 'true') {
+  app.use(utils.autoStoreData)
+  utils.addCheckedFunction(nunjucksAppEnv)
+}
 
 // Routes - may need importing from a main 'routes' script, rather than individually?
 app.use('/', startRoute)
